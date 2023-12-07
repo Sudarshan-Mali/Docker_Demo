ae8b2b51de617044670eb6bab259aedbe6375dce
Sudarshan <sudarshanmali@siddhatech.com>
2023-12-07 11:29:51 +0530

    Initial commit

---
+++ Scripts/LaunchURL/Script1701928077590.groovy
@@ -0,0 +1,23 @@
+import org.openqa.selenium.By
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+
+import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+
+WebUI.openBrowser('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+WebUI.waitForPageLoad(60)
+
+WebUI.maximizeWindow()
+
+WebDriver driver = DriverFactory.getWebDriver()
+WebElement VerfiyElement = driver.findElement(By.xpath("//img[@alt='company-branding']"))
+
+if (VerfiyElement.isDisplayed()) {
+	KeywordUtil.markPassed('Application Launched Successfully...!!')
+	driver.close()
+} else {
+	KeywordUtil.markFailed('Application Launched Failed...!!')
+}
